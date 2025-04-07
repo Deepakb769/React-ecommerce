7f9913e2d4c7bf6c06c2240648d37729b16c666e
Deepakb769 <deepakbohora01@gmail.com>
2025-04-07 17:49:21 +0530

    Folder structure for Ecommerce created

---
+++ ecom-react/src/pages/ProductPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Product from '../container/product'
+
+const ProductPage = () => {
+  return (
+    <div>
+      <Product />
+    </div>
+  )
+}
+
+export default ProductPage
